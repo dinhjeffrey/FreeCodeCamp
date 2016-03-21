a018eb4eeb3abff04ec411068aa2a4024c75e594
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-21 11:33:48 -0700

    added sylishCSS. update CSS location for html. added paramPamPam. create an express.js server that processes PUT messageid requests

---
+++ expressworks/paramPamPam.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+
+app.put('/message/:id', function(request, response) {
+	var id = request.params.id
+	var str = require('crypto')
+	.createHash('sha1')
+	.update(new Date().toDateString() + id)
+	.digest('hex')
+	response.send(str)
+})
+
+app.listen(process.argv[2])
+
