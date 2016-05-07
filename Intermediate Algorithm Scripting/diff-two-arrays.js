75efc1de3e64ba1fa2a789d4468a19e01054396d
jeffrey dinh <jeff@govyrl.io>
2016-05-07 12:57:43 -0700

    solved diff-two-arrays.js

---
+++ Intermediate Algorithm Scripting/diff-two-arrays.js
@@ -0,0 +1,11 @@
+
+var diffArray = (arr1, arr2) => {
+  // Same, same; but different.
+ var diff = arr1.filter( ele => {return arr2.indexOf(ele) === -1})
+ var diff2 = arr2.filter( ele => {return arr1.indexOf(ele) === -1})
+ 
+ return [...diff, ...diff2]
+
+}
+
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
