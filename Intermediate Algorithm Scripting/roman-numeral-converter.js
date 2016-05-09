dea4d7017efdded4eb3abd950e72e8d5849aa45e
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-05-09 10:03:03 -0700

    working from 1-10

--- Intermediate Algorithm Scripting/roman-numeral-converter.js
+++ Intermediate Algorithm Scripting/roman-numeral-converter.js
@@ -3,7 +3,29 @@ Convert the given number into a roman numeral.
 */
 
 var convertToRoman = (num) => {
- return num.split('');
+	var arr = num.toString().split('')
+	return arr.map( (ele) => {
+		// working from 1-10
+		if (ele==='1')
+			return 'I'
+		else if (ele==='2')
+			return 'II'
+		else if (ele==='3')
+			return 'III'
+		else if (ele==='4')
+			return 'IV'
+		else if (ele==='5')
+			return 'V'
+		else if (ele==='6')
+			return 'VI'
+		else if (ele==='7')
+			return 'VII'
+		else if (ele==='8')
+			return 'VIII'
+		else if (ele==='9')
+			return 'XI'
+		else return 'X'
+	} )
 }
 
-console.log(convertToRoman(36));
+console.log(convertToRoman(36))
