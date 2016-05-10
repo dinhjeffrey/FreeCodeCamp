875bc41bf2f1338e184851ea75fa9495675c6663
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-05-10 11:10:28 -0700

    updated todo on roman

--- Intermediate Algorithm Scripting/roman-numeral-converter.js
+++ Intermediate Algorithm Scripting/roman-numeral-converter.js
@@ -5,7 +5,7 @@ Convert the given number into a roman numeral.
 var convertToRoman = (num) => {
 	var arr = num.toString().split('')
 	return arr.map( (ele) => {
-		// working from 1-10
+		// working from 1-10. TODO: to make it work from 1-100
 		if (ele==='1')
 			return 'I'
 		else if (ele==='2')
